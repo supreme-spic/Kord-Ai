c5b41a61d074d8a1311ccf6cd1e7d180478bddd0
supreme-spic <techsupreme25@gmail.com>
2025-08-06 10:42:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2349067339193",
-    WORKTYPE: process.env.WORKTYPE || "private",
+    SESSION_ID: process.env.SESSION_ID || 'kord_ai-0b2VI6wPjSUx0vk0',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "263783458312",
+    WORKTYPE: process.env.WORKTYPE || "public",
     PREFIX: process.env.PREFIX || "[.]",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
     MONGODB_URI: process.env.MONGODB_URI || "",
